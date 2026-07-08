761bf99e6ce54b12a4af2990421192a2e7b4291f
LucasGrem <lucasgrem07@gmail.com>
2026-07-07 22:50:24 -0300

    feat: criei um gerador de json

--- BuscaCep/src/Main.java
+++ BuscaCep/src/Main.java
@@ -1,15 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
         ConsultaCep consulta = new ConsultaCep();
 
+        System.out.println("Digite um número de CEP para consulta: ");
+        var cep = leitura.nextLine();
+
         try {
-            Endereco novoEndereco = consulta.buscaEndereco("01001000");
+            Endereco novoEndereco = consulta.buscaEndereco(cep);
             System.out.println(novoEndereco);
+            GeradorArquivos gerador = new GeradorArquivos();
+            gerador.geraJson(novoEndereco);
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizando a aplicação...");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
